b4cc5e54f1a9bf8e50ba261065d466f8155d7929
Sergey Radchenko <avrelian@yandex.ru>
2015-07-25 22:53:01 +0300

    corrected displaying of colors in tests run through `protractor` (`colors` module should be installed - `npm install colors`)

--- specs/protractor/conf.js
+++ specs/protractor/conf.js
@@ -1,3 +1,6 @@
+var colors = require('colors');
+colors.enabled = true;
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['../nodejs/jasmine-custom-message.spec.js']
